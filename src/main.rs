ca079d22e55be8b06cb81d8b4d4fb6f233cd8b9e
Djkde01 <sergio.estrella@utp.edu.co>
2022-05-31 00:55:50 -0500

    feat(implemented division)

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     let re_add = Regex::new(r"(\d+)\s?\+\s?(\d+)").unwrap();
     let re_sub = Regex::new(r"(\d+)\s?\-\s?(\d+)").unwrap();
     let re_mul = Regex::new(r"(\d+)\s?\*\s?(\d+)").unwrap();
+    let re_div = Regex::new(r"(\d+)\s?/\s?(\d+)").unwrap();
 
     // User input
 
@@ -30,6 +31,25 @@ fn main() {
         let caps_num1 = caps.get(1).unwrap().as_str().parse::<i32>().unwrap();
         let caps_num2 = caps.get(2).unwrap().as_str().parse::<i32>().unwrap();
         let caps_result = caps_num1 * caps_num2;
+
+        input = input.replace(caps_expr, &caps_result.to_string());
+    }
+
+    // Division
+
+    loop {
+        let caps = re_div.captures(&input.as_str());
+
+        if caps.is_none() {
+            break;
+        }
+
+        let caps = caps.unwrap();
+
+        let caps_expr = caps.get(0).unwrap().as_str();
+        let caps_num1 = caps.get(1).unwrap().as_str().parse::<i32>().unwrap();
+        let caps_num2 = caps.get(2).unwrap().as_str().parse::<i32>().unwrap();
+        let caps_result = caps_num1 / caps_num2;
 
         input = input.replace(caps_expr, &caps_result.to_string());
     }
