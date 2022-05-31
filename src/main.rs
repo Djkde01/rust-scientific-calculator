31a0ef6cbfe20ad17b9cfbd0cf260a5baf8ba507
Djkde01 <sergio.estrella@utp.edu.co>
2022-05-31 00:48:07 -0500

    feat(Implemented multiplication)

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     // Regex
 
     let re_add = Regex::new(r"(\d+)\s?\+\s?(\d+)").unwrap();
+    let re_mul = Regex::new(r"(\d+)\s?\*\s?(\d+)").unwrap();
 
     // User input
 
@@ -12,6 +13,27 @@ fn main() {
     std::io::stdin().read_line(&mut input).unwrap();
 
     // Math operations
+
+    // Multiplication
+
+    loop {
+        let caps = re_mul.captures(&input.as_str());
+
+        if caps.is_none() {
+            break;
+        }
+
+        let caps = caps.unwrap();
+
+        let caps_expr = caps.get(0).unwrap().as_str();
+        let caps_num1 = caps.get(1).unwrap().as_str().parse::<i32>().unwrap();
+        let caps_num2 = caps.get(2).unwrap().as_str().parse::<i32>().unwrap();
+        let caps_result = caps_num1 * caps_num2;
+
+        input = input.replace(caps_expr, &caps_result.to_string());
+    }
+
+    // Addition
 
     loop {
         let caps = re_add.captures(input.as_str());
